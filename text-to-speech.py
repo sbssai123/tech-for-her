7d84e9b5f4fa8297088f2f20ffc9fc7ec5001207
sbssai123 <sbs.sai@gmail.com>
2018-09-01 17:28:57 -0400

    elena

---
+++ text-to-speech.py
@@ -0,0 +1,32 @@
+"""Synthesizes speech from the input string of text or ssml.
+
+Note: ssml must be well-formed according to:
+    https://www.w3.org/TR/speech-synthesis/
+"""
+from google.cloud import texttospeech
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.types.SynthesisInput(text="Hi, my name is Elena! I am the Head of Data Science at AirBnB. Here is my journey to becoming a data scientist and resources for starting this career path as a young woman.")
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.types.VoiceSelectionParams(
+    language_code='en-US',
+    ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.types.AudioConfig(
+    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+# The response's audio_content is binary.
+with open('output.mp3', 'wb') as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
