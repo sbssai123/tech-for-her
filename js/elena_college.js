9179b148898c5e7c2c44e907ec321fbe44ae3130
sbssai123 <sbs.sai@gmail.com>
2018-09-02 02:34:08 -0400

    new pages ds

--- js/elena_college.js
+++ js/elena_college.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
     $("#first").fadeIn(2000, function(){
         $("#second").fadeIn(5000, function(){
-            $("#third").fadeIn(5000, function(){
-                $("#fourth").fadeIn(5000, function(){
-                    $("#arrow").show();
-                    $("#continue").show();
-                });
+            $("#third").fadeIn(5000);
+            $("#fourth").fadeIn(5000, function(){
+                $("#arrow").show();
+                $("#continue").show();
             });
         });
     });
--- text-to-speech.py
+++ text-to-speech.py
@@ -9,7 +9,7 @@ from google.cloud import texttospeech
 client = texttospeech.TextToSpeechClient()
 
 # Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text= "I did not take the traditional path to becoming a data scientist. I studied ethics, politics, and economics and earned a P H D in Education at Stanford. My dissertation was on studying friend networks. This was when I realized that I needed to learn computer science. Click on the school to get resources to get started on data science.")
+synthesis_input = texttospeech.types.SynthesisInput(text= "Here is the day in the life for someone like me. I participate in data scraping, cleaning, and storage. After data is preprocessed, I train machine learning models with this data and am able to make predictions. These models can be used to predict medical diagnosis, stock prices, and more. About seventy percent of my time is spent shaping data.")
 
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
@@ -26,7 +26,7 @@ audio_config = texttospeech.types.AudioConfig(
 response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
 # The response's audio_content is binary.
-with open('elena_college.mp3', 'wb') as out:
+with open('elena_dil.mp3', 'wb') as out:
     # Write the response to the output file.t
     out.write(response.audio_content)
     print('Audio content written to file "output.mp3"')
