b1729397f3a7ab6d80f012889e4a2e384c89a449
sbssai123 <sbs.sai@gmail.com>
2018-09-01 22:04:59 -0400

    created new pages

---
+++ js/elena_college.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#first").fadeIn(2000, function(){
+        $("#second").fadeIn(5000, function(){
+            $("#third").fadeIn(5000, function(){
+                $("#fourth").fadeIn(5000, function(){
+                    $("#arrow").show();
+                    $("#continue").show();
+                });
+            });
+        });
+    });
+});
--- text-to-speech.py
+++ text-to-speech.py
@@ -9,7 +9,7 @@ from google.cloud import texttospeech
 client = texttospeech.TextToSpeechClient()
 
 # Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text="Hi, my name is Elena! I am the Head of Data Science at AirBnB. Here is my journey to becoming a data scientist and resources for starting this career path as a young woman.")
+synthesis_input = texttospeech.types.SynthesisInput(text= "I did not take the traditional path to becoming a data scientist. I studied ethics, politics, and economics and earned a P H D in Education at Stanford. My dissertation was on studying friend networks. This was when I realized that I needed to learn computer science. Click on the school to get resources to get started on data science.")
 
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
@@ -26,7 +26,7 @@ audio_config = texttospeech.types.AudioConfig(
 response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
 # The response's audio_content is binary.
-with open('output.mp3', 'wb') as out:
-    # Write the response to the output file.
+with open('elena_college.mp3', 'wb') as out:
+    # Write the response to the output file.t
     out.write(response.audio_content)
     print('Audio content written to file "output.mp3"')
