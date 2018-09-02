d9f57716a65dacaab3252e75d2552fef7db66ab3
sbssai123 <sbs.sai@gmail.com>
2018-09-02 07:02:39 -0400

    styling fixes

--- text-to-speech.py
+++ text-to-speech.py
@@ -9,12 +9,12 @@ from google.cloud import texttospeech
 client = texttospeech.TextToSpeechClient()
 
 # Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text= "Here is the day in the life for someone like me. I participate in data scraping, cleaning, and storage. After data is preprocessed, I train machine learning models with this data and am able to make predictions. These models can be used to predict medical diagnosis, stock prices, and more. About seventy percent of my time is spent shaping data.")
+synthesis_input = texttospeech.types.SynthesisInput(text= "Hello, I am Kim Swift! I am very well known for being a great video game designer. I'm going to show you my typical day as a game developer.")
 
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
 voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
+    language_code='en-US-Standard-E',
     ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
 
 # Select the type of audio file you want returned
@@ -26,7 +26,7 @@ audio_config = texttospeech.types.AudioConfig(
 response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
 # The response's audio_content is binary.
-with open('elena_dil.mp3', 'wb') as out:
+with open('kim_intro.mp3', 'wb') as out:
     # Write the response to the output file.t
     out.write(response.audio_content)
     print('Audio content written to file "output.mp3"')
